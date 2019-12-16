0f87c47d22d4549bbc76f5327ee0518e3a71c54d
Álvaro García Gutiérrez <alvaro.garcia.gutierrez@alumnos.upm.es>
2019-12-16 19:38:47 +0100

    card methods>

--- src/main/java/es/upm/pproject/prePaidCard/Card.java
+++ src/main/java/es/upm/pproject/prePaidCard/Card.java
@@ -1,4 +1,68 @@
 package es.upm.pproject.prePaidCard;
 
+import java.util.Date;
+
 public class Card {
+
+	long IDnumber;
+	Date expirationDate;
+	int balance;
+	int pin;													//in the user?
+	
+	//constructor
+	public Card(long IDnumber, int balance, int pin) {
+		
+		this.IDnumber = IDnumber;
+		this.balance = balance;
+		this.pin = pin;											//add hash function
+		expirationDate = new Date();							//Today´s date
+		expirationDate.setYear(expirationDate.getYear()+1);		//Plus one year
+		
+	}
+	
+	
+	public int consultBalance (int pin) {
+		
+		if (this.pin == pin) {
+			return balance;
+		}
+		return -1;
+	
+	}
+	
+	//method to charge some amount of money
+	public int charge (int pin, int amount) {
+		
+		if (this.pin == pin) {
+			balance = balance + amount;
+			return balance;
+		}
+		return -1;
+		
+		
+	}
+	
+	//method to pay some amount of money
+	public int pay (int pin, int amount) {
+		
+		if (this.pin == pin) {
+			if (balance >= amount) {	
+				balance = balance - amount;
+				return balance;
+			}
+		}
+		return -1;
+	}
+	
+	//method to change the pin
+	public boolean changePin (int oldPin, int newPin) {
+		
+		if (this.pin == oldPin) {
+			this.pin = newPin;
+			return true;
+		}
+		return false;	
+		
+	}
+	
 }
--- src/main/java/es/upm/pproject/prePaidCard/PrePaidCardManager.java
+++ src/main/java/es/upm/pproject/prePaidCard/PrePaidCardManager.java
@@ -4,16 +4,14 @@ package es.upm.pproject.prePaidCard;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- * Hello world!
- *
- */
 public class PrePaidCardManager implements PrePaidCardInterface {
 
-    HashMap<Integer, ArrayList<Card>> mapa = new HashMap<>();
+    HashMap<User, ArrayList<Card>> mapa = new HashMap<>();
 
     public PrePaidCardManager() {
-
+    	
+    	
+    	
     }
 
 
