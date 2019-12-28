52ae12c6f55855f93fe9c0916ddfe4ec88e551ea
Javier Galarza <javier.galarza12@gmail.com>
2019-12-28 20:56:00 +0100

    Fixed #24 Developed methods to load cards from a file. Created the storage file in the root of the project, called .data.json

--- src/main/java/es/upm/pproject/prePaidCard/model/PrePaidCardManager.java
+++ src/main/java/es/upm/pproject/prePaidCard/model/PrePaidCardManager.java
@@ -1,7 +1,20 @@
 package es.upm.pproject.prePaidCard.model;
 
 
+import java.sql.PreparedStatement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.File;
+
 
 
 public class PrePaidCardManager implements PrePaidCardInterface {
@@ -11,30 +24,34 @@ public class PrePaidCardManager implements PrePaidCardInterface {
 
 
     public PrePaidCardManager() {
-
+		readJsonFromFile();
     }
+    public static void main(String [] args) {
+		PrePaidCardManager pre = new PrePaidCardManager();
+	}
 
 
     // method to register a new card for a user
-    public void buyCard(String owner, Integer balance, String pin) {   //change to boolean?
-		  Card card = new Card(cardNumber, balance, pin, owner);
-		  cards.put(cardNumber, card);
-		  cardNumber++;
+    public void buyCard(String owner, Long balance, String pin) {   //change to boolean?
+		Date expirationDate = new Date();
+		expirationDate.setYear(expirationDate.getYear()+1);
+		Card card = new Card(cardNumber, balance, pin, owner, expirationDate);
+		cards.put(cardNumber, card);
+		cardNumber++;
     }
 
     // method to charge money in a card
-    public Integer chargeCard(Long idNumber, String pin, Integer amount) throws CardDoesntExistException, ExpiredCardException, WrongPINException {
+    public Long chargeCard(Long idNumber, String pin, Integer amount) throws CardDoesntExistException, ExpiredCardException, WrongPINException {
     	if (!cards.containsKey(idNumber)) {
     		throw new CardDoesntExistException();
 		}
 
 		Card card = cards.get(idNumber);
 		return card.charge(pin, amount);
-
     }
 
     // method pay with a card
-    public Integer payCard(Long idNumber, String pin, Integer amount) throws CardDoesntExistException, ExpiredCardException, NotEnoughMoneyException, WrongPINException {
+    public Long payCard(Long idNumber, String pin, Integer amount) throws CardDoesntExistException, ExpiredCardException, NotEnoughMoneyException, WrongPINException {
   		if (!cards.containsKey(idNumber)) {
   			throw new CardDoesntExistException();
   		}
@@ -66,7 +83,7 @@ public class PrePaidCardManager implements PrePaidCardInterface {
   	}
 
     // method to consult the balance of a card
-  	public Integer consultBalance(Long idNumber, String pin) throws CardDoesntExistException, WrongPINException {
+  	public Long consultBalance(Long idNumber, String pin) throws CardDoesntExistException, WrongPINException {
   		if (!cards.containsKey(idNumber)) {
   			throw new CardDoesntExistException();
   		}
@@ -78,4 +95,42 @@ public class PrePaidCardManager implements PrePaidCardInterface {
   	public HashMap<Long, Card> getCards() {
   		return cards;
   	}
+
+	private void readJsonFromFile() {
+		//JSON parser object to parse read file
+		JSONParser jsonParser = new JSONParser();
+		String filePath = new File("").getAbsolutePath();
+
+		try (FileReader reader = new FileReader(filePath.concat("/.data.json")))
+		{
+			//Read JSON file
+			Object obj = jsonParser.parse(reader);
+			JSONArray jsonfile = (JSONArray) obj;
+			parseCards(jsonfile);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (java.text.ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void parseCards(JSONArray cards) throws java.text.ParseException {
+		for(int i = 0; i < cards.size(); i++) {
+			JSONObject card = (JSONObject) cards.get(i);
+			Long cardNumber = (Long) card.get("number");
+			String owner = (String) card.get("owner");
+			String pin = (String) card.get("pin");
+			Long balance = (Long) card.get("balance");
+			String date = (String) card.get("expDate");
+			Date expirationDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+
+			Card storedCard = new Card(cardNumber, balance, pin, owner, expirationDate);
+			this.cards.put(cardNumber, storedCard);
+		}
+	}
 }
